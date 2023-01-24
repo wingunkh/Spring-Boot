6b367cef049142882677e29b9e48b2a8a067f52e
wingunkh <99gusrms@naver.com>
2023-01-24 15:26:33 +0900

    섹션 2. 스프링 웹 개발 기초 - MVC와 템플릿 엔진

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,12 +3,19 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @GetMapping("hello")
+    @GetMapping("hello") //http://localhost:8080/hello
     public String method(Model model){
-        model.addAttribute("data", "spring!!!");
-        return "hello"; //컨트롤러에서 리턴 값으로 문자를 반환하면 뷰 리졸버가 화면을 찾아서 처리한다.
+        model.addAttribute("data", "hello!!!"); //키와 값
+        return "hello"; //컨트롤러에서 리턴 값으로 문자열을 반환하면 뷰 리졸버가 화면을 찾아서 처리한다.
+    }
+
+    @GetMapping("hello-mvc") //http://localhost:8080/hello-mvc?name=
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-templates";
     }
 }
