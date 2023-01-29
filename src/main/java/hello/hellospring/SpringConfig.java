20daf59cc2447c28a04134896f4e43fabf5844bc
wingunkh <99gusrms@naver.com>
2023-01-29 19:27:42 +0900

    섹션 5. 회원 관리 예제(웹 MVC 개발) - 회원 웹 기능(홈 화면 추가)

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -6,7 +6,7 @@ import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration //자바 코드로 직접 스프링 빈을 등록하는 방법
+// @Configuration //자바 코드로 직접 스프링 빈을 등록하는 방법
 public class SpringConfig {
     @Bean
     public MemberService memberService() {
