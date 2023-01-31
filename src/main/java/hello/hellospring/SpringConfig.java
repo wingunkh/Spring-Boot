de6fc19374e387f5a88a3b0fe65c45c3579502da
wingunkh <99gusrms@naver.com>
2023-01-31 15:55:34 +0900

    섹션 6. 스프링 DB 접근 기술 - 순수 JDBC

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-// @Configuration //자바 코드로 직접 스프링 빈을 등록하는 방법
+import javax.sql.DataSource;
+
+@Configuration //자바 코드로 직접 스프링 빈을 등록하는 방법
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired //객체 생성 시점에 스프링 컨테이너에서 해당 스프링 빈을 찾아서 주입
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service //스프링 빈으로 자동 등록
+// @Service //스프링 빈으로 자동 등록
 public class MemberService {
     private final MemberRepository memberRepository;
 
